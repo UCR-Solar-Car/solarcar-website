23ddcd986c84086e3b32997e80d0af10c0670170
Divyank Shah <divyank.shah.2016@gmail.com>
2022-12-12 21:17:22 -0800

    add header

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,10 @@ import Header from "../components/Header";
 const about = () => {
   return (
     <div>
-      <Header />
+      <Header
+        image="https://ieee.ucr.edu/sites/default/files/styles/scale_367/public/2lt0u4l.jpg?itok=8IIhrRvt"
+        title="About Us"
+      />
       <AboutTiles />
       <AboutParagraph />
       <Gallery />
