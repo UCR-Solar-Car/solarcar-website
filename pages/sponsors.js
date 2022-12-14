2a46bf89ae825f667c31fcfc930ec6a7e887e0c6
Divyank Shah <divyank.shah.2016@gmail.com>
2022-12-14 00:34:08 -0800

    sponsors page

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
+import SponsorOutreach from "../components/SponsorOutreach";
+import Sponsors from "../components/Sponsors";
 
 const sponsors = () => {
   return (
@@ -8,6 +10,8 @@ const sponsors = () => {
         image="https://ieee.ucr.edu/sites/default/files/styles/scale_367/public/2lt0u4l.jpg?itok=8IIhrRvt"
         title="Sponsors"
       />
+      <Sponsors />
+      <SponsorOutreach />
     </div>
   );
 };
