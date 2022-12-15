f7e95428e832c3beca725fe85ce27c64f986cd8e
Aarav Sane <aaravtennis@gmail.com>
2022-12-15 19:53:07 +0530

    fixed build errors

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { AboutParagraph } from "../components/AboutParagraph";
-import { AboutTiles } from "../components/AboutTiles";
-import { Gallery } from "../components/Gallery";
-import { Header } from "../components/Header";
+import AboutParagraph from "../components/AboutParagraph";
+import AboutTiles from "../components/AboutTiles";
+import Gallery from "../components/Gallery";
+import Header from "../components/Header";
 
 const about = () => {
   return (
