635e3f853c2c985e9437247c94271f6cfa9ab907
Divyank Shah <divyank.shah.2016@gmail.com>
2022-12-15 22:04:55 -0800

    setup

--- components/Team.jsx
+++ components/Team.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Members = () => {
-  return <div>Members</div>;
+const Team = ({ team }) => {
+  return (
+    <div>
+      <div className="text-solar-white">HEADER</div>
+      <div>
+        {team.map((member, index) => (
+          <p className="text-solar-white" key={index}>
+            {member}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Members;
+export default Team;
--- components/Teams.jsx
+++ components/Teams.jsx
@@ -2,25 +2,63 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import Team from "./Team";
 
+const teams = [
+  [
+    "ELECTRICAL",
+    "ELECTRICAL",
+    "ELECTRICAL",
+    "ELECTRICAL",
+    "ELECTRICAL",
+    "ELECTRICAL",
+  ],
+  [
+    "MECHANICAL",
+    "MECHANICAL",
+    "MECHANICAL",
+    "MECHANICAL",
+    "MECHANICAL",
+    "MECHANICAL",
+    "MECHANICAL",
+  ],
+  [
+    "SOFTWARE",
+    "SOFTWARE",
+    "SOFTWARE",
+    "SOFTWARE",
+    "SOFTWARE",
+    "SOFTWARE",
+    "SOFTWARE",
+  ],
+  [
+    "TELEMETRY",
+    "TELEMETRY",
+    "TELEMETRY",
+    "TELEMETRY",
+    "TELEMETRY",
+    "TELEMETRY",
+    "TELEMETRY",
+  ],
+  [
+    "BUSINESS",
+    "BUSINESS",
+    "BUSINESS",
+    "BUSINESS",
+    "BUSINESS",
+    "BUSINESS",
+    "BUSINESS",
+  ],
+];
+
 const Teams = () => {
   return (
     <div>
       <Row>
-        <Col>
-          <Team />
-        </Col>
-        <Col>
-          <Team />
-        </Col>
-        <Col>
-          <Team />
-        </Col>
-        <Col>
-          <Team />
-        </Col>
-        <Col>
-          <Team />
-        </Col>
+        {teams.map((team, index) => (
+          <Col key={index}>
+            {console.log("TEAMS", team)}
+            <Team team={team} />
+          </Col>
+        ))}
       </Row>
     </div>
   );
