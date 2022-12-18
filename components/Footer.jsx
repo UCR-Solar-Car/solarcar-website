438b299ee389fc37a2999e034708c79f05cb0f0a
Jason Lin <jason33771@gmail.com>
2022-12-18 13:37:57 -0800

    Finished Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React from 'react'
+import {FaFacebookSquare as FaceBookIcon} from 'react-icons/fa';
+import {FaDiscord as DiscordIcon} from 'react-icons/fa';
+import {FaInstagram as InstagramIcon} from 'react-icons/fa';
+import {FaLinkedin as LinkedinIcon} from 'react-icons/fa';
+import {FaEnvelope as EmailIcon} from 'react-icons/fa';
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
 
-export default Footer;
+function footer() {
+  return (
+    <div className ='flex w-[100%] h-auto bg-[#1F1E21] justify-center border-t-8 border-[#FEC155]' >
+        <div className ='flex text-white space-x-12 m-2 text-[50px]'>
+            <a href='https://www.facebook.com/UCRSolarCar/'>
+              <FaceBookIcon/>
+            </a>
+            <a href=''>
+              <DiscordIcon/>
+            </a>
+            <a href='https://www.instagram.com/ucrsolarcar/?hl=en'>
+              <InstagramIcon/>
+            </a>
+            <a href='https://www.linkedin.com/company/ucr-solar-car'>
+              <LinkedinIcon/>
+            </a>
+            <a href=''>
+              <EmailIcon/>
+            </a>
+        </div>
+    </div>
+  )
+}
+
+export default footer
