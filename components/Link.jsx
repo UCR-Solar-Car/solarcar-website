8b72ef9ef477488060e68d38f0661bcc294bd2cf
Aryan-Kanuparti <aryan.kanuparti@gmail.com>
2022-12-18 16:24:54 -0800

    worked more close to finalizing format

--- components/Link.jsx
+++ components/Link.jsx
@@ -6,40 +6,45 @@ import React from "react";
 
 // export default Link;
 
-
-function YellowRectangleWithButton() {
+const Link = () => {
   return (
-    <div className='bg-#FEC155 h-331 w-358 relative'>
+    <div className="bg-[#FEC155] h-331 w-358 relative block">
       <div
-        className='text-center mt-30'
+        className="text-center mt-100 p-6"
         style={{
-          fontFamily: 'Raleway',
-          fontSize: '48px',
-          fontWeight: '400',
-          color: '#1F1E21',
-          lineHeight: '56.35px',
+          fontFamily: "Raleway",
+          fontSize: "38px",
+          fontWeight: "400",
+          color: "#1F1E21",
+          lineHeight: "56.35px",
+          width: '100%', // Add this line
         }}
       >
         Project Manager
       </div>
       <div
-        className='text-center absolute bottom-0 left-0 right-0 mb-33'
+        // className="text-center absolute bottom-0 left-0 right-0 mb-33"
         style={{
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          width: '232px',
-          height: '94px',
+          backgroundColor: "#2A363D",
+          marginLeft: "auto",
+          marginRight: "auto",
+          width: "160px",
+          height: "60px",
+          // padding: "40px"
         }}
       >
-        <a href='https://ieee.ucr.edu/projects/solar-car' className='text-blue-500'>
-          <button className='text-white bg-#2A363D hover:bg-blue-700 w-full h-full'>
+        <a
+          href="https://ieee.ucr.edu/projects/solar-car"
+          className="text-blue-500"
+        >
+          <button className="text-white bg-#2A363D hover:bg-blue-700 w-full h-full p-2">
             <div
               style={{
-                fontFamily: 'Josefin Sans',
-                fontSize: '40px',
-                lineHeight: '40px',
-                textAlign: 'center',
-                color: '#FFD680',
+                fontFamily: "Josefin Sans",
+                fontSize: "35px",
+                lineHeight: "40px",
+                textAlign: "center",
+                color: "#FFD680",
               }}
             >
               APPLY
@@ -49,6 +54,7 @@ function YellowRectangleWithButton() {
       </div>
     </div>
   );
-}
+};
 
-export default YellowRectangleWithButton;
+
+export default Link;
