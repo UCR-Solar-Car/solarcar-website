2da17df397b8c711c969137690f7fd301579219a
Aryan-Kanuparti <aryan.kanuparti@gmail.com>
2022-12-20 15:41:33 -0800

    more formatting changes converting from tailwind to css adn trying to preserve the same visuals

--- components/Link.jsx
+++ components/Link.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Link = () => {
   return (
-    <div className="bg-solar-gold">
+    <div className="bg-solar-gold h-331 w-358 ">
       <div className="text-center mt-100 p-6 font-raleway text-3xl text-solar-black">
         Project Manager
       </div>
-      <div
+      <div class="box-border w-232 h-105 bg-gray mt-100 pb-8 px-8 pt-6 p-9  justify-center items-center">
+        {/* <div
         style={{
           backgroundColor: "#2A363D",
           marginLeft: "auto",
@@ -14,13 +15,14 @@ const Link = () => {
           width: "160px",
           height: "60px",
         }}
-      >
+      > */}
         <a
           href="https://ieee.ucr.edu/projects/solar-car"
           className="text-blue-500"
         >
-          <button className="text-white bg-#2A363D hover:bg-blue-700 w-full h-full p-2.5">
-            <div
+          <button className="text-white bg-solar-gray hover:bg-blue-700 w-full h-full p-2.5">
+            <div applyText="leading-10 text-center font-josefin text-solar-yellow font-normal text-3xl">
+              {/* <div
               style={{
                 fontFamily: "Josefin Sans",
                 fontSize: "28px",
@@ -28,7 +30,7 @@ const Link = () => {
                 textAlign: "center",
                 color: "#FFD680",
               }}
-            >
+            > */}
               APPLY
             </div>
           </button>
