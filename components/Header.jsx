b6d4f77eb57b197865a3be75b1c00eee70712df0
Divyank Shah <divyank.shah.2016@gmail.com>
2022-12-22 01:13:42 -0800

    setup

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,7 +12,7 @@ const Header = ({ image, title }) => {
         <div className="bg-gradient-to-b from-white to-black p-2 absolute top-0 left-0 w-full h-full opacity-50"></div>
       </div>
 
-      <div className="flex justify-center items-center absolute bottom-[10%] left-1/2 -translate-x-1/2 ">
+      <div className="flex justify-center items-center absolute bottom-[10%] w-full left-1/2 -translate-x-1/2 ">
         <p className="font-raleway text-3xl text-solar-white m-0">
           {">> "}
           {title}
--- pages/recruitment.js
+++ pages/recruitment.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
+import Subteams from "../components/Subteams";
 
 const recruitment = () => {
   return (
@@ -8,6 +9,7 @@ const recruitment = () => {
         image="https://ieee.ucr.edu/sites/default/files/styles/scale_367/public/2lt0u4l.jpg?itok=8IIhrRvt"
         title="Recruitment"
       />
+      <Subteams />
     </div>
   );
 };
