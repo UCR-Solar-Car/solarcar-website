45bf97eaeeb75dfc47e4aea2aab405611749256a
menthy-wu <wumengxuan0728@gmail.com>
2022-12-26 15:17:39 -0800

    Alternate Link Colors

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Link = () => {
+const Link = ({backgroundColor}) => {
   return (
-    <div className="bg-solar-gold h-331 w-358 ">
+    <div className={`bg-${backgroundColor} h-331 w-358 `}>
       <div className="text-center mt-100 p-6 font-raleway text-3xl text-solar-black">
         Project Manager
       </div>
-      <div class="box-border w-232 h-105 bg-gray mt-100 pb-8 px-8 pt-6 p-9  justify-center items-center">
+      <div className="box-border w-232 h-105 bg-gray mt-100 pb-8 px-8 pt-6 p-9  justify-center items-center">
         {/* <div
         style={{
           backgroundColor: "#2A363D",
@@ -21,16 +21,7 @@ const Link = () => {
           className="text-blue-500"
         >
           <button className="text-white bg-solar-gray hover:bg-blue-700 w-full h-full p-2.5">
-            <div applyText="leading-10 text-center font-josefin text-solar-yellow font-normal text-3xl">
-              {/* <div
-              style={{
-                fontFamily: "Josefin Sans",
-                fontSize: "28px",
-                lineHeight: "20px",
-                textAlign: "center",
-                color: "#FFD680",
-              }}
-            > */}
+            <div className={`text-center font-josefin text-${backgroundColor} text-2xl`}>
               APPLY
             </div>
           </button>
--- components/Links.jsx
+++ components/Links.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import Link from "./Link";
 
 const links = new Array(10).fill("");
 
 const Links = () => {
+  const [screenSize, setScreenSize] = useState("");
+  useEffect(() => {
+    addEventListener("resize", () => {
+      setScreenSize(
+        window.innerWidth >= 992 ? "lg" : window.innerWidth >= 576 ? "sm" : "xs"
+      );
+      console.log(screenSize);
+    });
+  }, []);
   return (
     <div className="flex justify-center items-center">
       <Row className="w-10/12">
         {links.map((link, index) => (
-          <Col key={index} xl={3} className="p-2">
-            <Link />
+          <Col key={index} xs={6} sm={4} lg={3} className="p-2">
+            {screenSize == "xs" ? (
+              <Link
+                backgroundColor={
+                  Math.floor((index + 1) / 2) % 2 == 0
+                    ? "solar-blue "
+                    : "solar-gold "
+                }
+              />
+            ) : screenSize == "lg" && index % 8 < 4 ? (
+              <Link
+                backgroundColor={index % 2 == 0 ? "solar-blue " : "solar-gold "}
+              />
+            ) : (
+              <Link
+                backgroundColor={index % 2 != 0 ? "solar-blue " : "solar-gold "}
+              />
+            )}
           </Col>
         ))}
       </Row>
