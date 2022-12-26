f3d35169116f17e4052d608320d85c6c53fe3b47
Aryan-Kanuparti <aryan.kanuparti@gmail.com>
2022-12-26 13:51:51 -0800

    work on issue #60 did input validation for cell number, phone number, and email format, and made all fields requried

--- components/AdoptInput.jsx
+++ components/AdoptInput.jsx
@@ -14,12 +14,42 @@ const AdoptInput = () => {
   const [data, setData] = useState(user);
 
   const handleTyping = (e) => {
+    //was the validation supposed to be here instead like while users are typing?//
     setData({ ...data, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = () => {
-    console.log("Submit Pressed");
+    //currently set up validation for submission, can type anything in the boxes
+    // Input validation for cell number field
+    if (
+      Number.isInteger(Number(data.cell)) &&
+      data.cell >= 1 &&
+      data.cell <= 255
+    ) {
+      console.log("Submit Pressed: Cell number is valid");
+    } else {
+      console.log("Cell number is not valid");
+    }
+
+    // Input validation for phone number field
+    if (data.phone.length === 10) {
+      console.log("Submit Pressed: Phone number is valid");
+    } else {
+      console.log("Phone number is not valid");
+    }
+
+    // Input validation for email field
+    const emailRegex = /^[^\s@] + @[^\s@] + \.[^\s@] + $/;
+    if (emailRegex.test(data.email)) {
+      console.log("Submit Pressed: Email is valid");
+    } else {
+      console.log("Email is not valid");
+    }
   };
+
+  // const handleSubmit = () => {
+  //   console.log("Submit Pressed");
+  // };
 
   return (
     <div className="bg-solar-black text-white">
@@ -34,6 +64,7 @@ const AdoptInput = () => {
         </Col>
         <Col xs={6} className="text-left m-0 p-0">
           <input
+            required
             className="text-solar-blue font-raleway py-1 px-2 mx-4 my-2 focus:outline-none placeholder:text-solar-blue"
             name="cell"
             type="number"
@@ -52,6 +83,7 @@ const AdoptInput = () => {
         </Col>
         <Col xs={6} className="text-left m-0 p-0">
           <input
+            required
             className="text-solar-blue font-raleway py-1 px-2 mx-4 my-2 focus:outline-none placeholder:text-solar-blue"
             name="name"
             type="text"
@@ -70,8 +102,9 @@ const AdoptInput = () => {
         </Col>
         <Col xs={6} className="text-left m-0 p-0">
           <input
+            required
             className="text-solar-blue font-raleway py-1 px-2 mx-4 my-2 focus:outline-none placeholder:text-solar-blue"
-            name="name"
+            name="email"
             type="email"
             onChange={handleTyping}
             value={data.email}
@@ -88,8 +121,9 @@ const AdoptInput = () => {
         </Col>
         <Col xs={6} className="text-left m-0 p-0">
           <input
+            required
             className="text-solar-blue font-raleway py-1 px-2 mx-4 my-2 focus:outline-none placeholder:text-solar-blue"
-            name="name"
+            name="phone"
             type="tel"
             onChange={handleTyping}
             value={data.phone}
@@ -106,8 +140,9 @@ const AdoptInput = () => {
         </Col>
         <Col xs={6} className="text-left m-0 p-0">
           <input
+            required
             className="text-solar-blue font-raleway py-1 px-2 mx-4 my-2 focus:outline-none placeholder:text-solar-blue"
-            name="name"
+            name="paypal"
             type="text"
             onChange={handleTyping}
             value={data.paypal}
