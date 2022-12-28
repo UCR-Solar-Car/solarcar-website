8c19891f057524819f48814b76a94972ffc7eaf8
mgolw001 <mgolw001@ucr.edu>
2022-12-28 12:03:43 -0800

    removed buttons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,73 +6,64 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaEnvelope } from "react-icons/fa";
 import { motion } from "framer-motion";
 
-const openInNewTab = (url) => {
-  window.open(url, "_blank", "noopener,noreferrer");
-};
-
 function footer() {
   return (
     <div className="flex w-full bg-solar-black justify-center py-8">
       <div className="flex justify-evenly items-center w-3/4 sm:w-1/2 text-5xl">
-        <button
-          onClick={() => openInNewTab("https://www.facebook.com/UCRSolarCar/")}
+        <motion.a
+          whileHover={{ scale: 1.2 }}
+          transition={{ type: "spring", stiffness: 500 }}
+          href="https://www.facebook.com/UCRSolarCar/"
+          target="_blank"
+          noreferrer="noopener"
+          className="text-solar-white hover:text-solar-gold"
         >
-          <motion.a
-            whileHover={{ scale: 1.2 }}
-            transition={{ type: "spring", stiffness: 500 }}
-            className="text-solar-white hover:text-solar-gold"
-          >
-            <FaFacebookSquare />
-          </motion.a>
-        </button>
+          <FaFacebookSquare />
+        </motion.a>
 
-        <button onClick={() => openInNewTab("https://discord.gg/eGskEcjNvb")}>
-          <motion.a
-            whileHover={{ scale: 1.2 }}
-            transition={{ type: "spring", stiffness: 500 }}
-            className="text-solar-white hover:text-solar-gold"
-          >
-            <FaDiscord />
-          </motion.a>
-        </button>
-
-        <button
-          onClick={() =>
-            openInNewTab("https://www.instagram.com/ucrsolarcar/?hl=en")
-          }
+        <motion.a
+          whileHover={{ scale: 1.2 }}
+          transition={{ type: "spring", stiffness: 500 }}
+          href="https://discord.gg/eGskEcjNvb"
+          target="_blank"
+          noreferrer="noopener"
+          className="text-solar-white hover:text-solar-gold"
         >
-          <motion.a
-            whileHover={{ scale: 1.2 }}
-            transition={{ type: "spring", stiffness: 500 }}
-            className="text-solar-white hover:text-solar-gold"
-          >
-            <FaInstagram />
-          </motion.a>
-        </button>
+          <FaDiscord />
+        </motion.a>
 
-        <button
-          onClick={() =>
-            openInNewTab("https://www.linkedin.com/company/ucr-solar-car")
-          }
+        <motion.a
+          whileHover={{ scale: 1.2 }}
+          transition={{ type: "spring", stiffness: 500 }}
+          href="https://www.instagram.com/ucrsolarcar/?hl=en"
+          target="_blank"
+          noreferrer="noopener"
+          className="text-solar-white hover:text-solar-gold"
         >
-          <motion.a
-            whileHover={{ scale: 1.2 }}
-            transition={{ type: "spring", stiffness: 500 }}
-            className="text-solar-white hover:text-solar-gold"
-          >
-            <FaLinkedin />
-          </motion.a>
-        </button>
+          <FaInstagram />
+        </motion.a>
 
-        <button onClick={() => openInNewTab("mailto:ucrsolarcar@gmail.com")}>
-          <motion.a
-            whileHover={{ scale: 1.2 }}
-            transition={{ type: "spring", stiffness: 500 }}
-            className="text-solar-white hover:text-solar-gold"
-          >
-            <FaEnvelope />
-          </motion.a>
-        </button>
+        <motion.a
+          whileHover={{ scale: 1.2 }}
+          transition={{ type: "spring", stiffness: 500 }}
+          href="https://www.linkedin.com/company/ucr-solar-car"
+          target="_blank"
+          noreferrer="noopener"
+          className="text-solar-white hover:text-solar-gold"
+        >
+          <FaLinkedin />
+        </motion.a>
+
+        <motion.a
+          whileHover={{ scale: 1.2 }}
+          transition={{ type: "spring", stiffness: 500 }}
+          href="mailto:ucrsolarcar@gmail.com"
+          target="_blank"
+          noreferrer="noopener"
+          className="text-solar-white hover:text-solar-gold"
+        >
+          <FaEnvelope />
+        </motion.a>
       </div>
     </div>
   );
