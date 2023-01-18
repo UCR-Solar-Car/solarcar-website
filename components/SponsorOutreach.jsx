c50548e4ecde2852430e8cbca21521f0260d6c68
Justin Yee <justiny@solidprofessor.com>
2023-01-17 22:48:22 -0800

    linked Become a Sponsor

--- components/SponsorOutreach.jsx
+++ components/SponsorOutreach.jsx
@@ -7,9 +7,14 @@ const SponsorOutreach = () => {
         Interested in becoming a sponser? <br />
         Learn more below!
         <br />
-        <button className="border-2 border-solar-white hover:bg-solar-blue px-12 py-2 rounded-full mt-8">
-          BECOME A SPONSOR {">>"}
-        </button>
+        <a
+          href="https://drive.google.com/file/d/1nNnRxFIXaLZs-PQoyrhYBkaeCeF8jw0S/view"
+          className="text-solar-white"
+        >
+          <button className="border-2 border-solar-white hover:bg-solar-blue px-12 py-2 rounded-full mt-8 text-solar-white">
+            BECOME A SPONSOR {">>"}
+          </button>
+        </a>
       </div>
     </div>
   );
