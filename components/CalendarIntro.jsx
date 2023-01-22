4e74acd8f94fd1acfb5d4ae41a2ea3382c1a235c
ysh0305 <yhung022@ucr.edu>
2023-01-22 14:48:36 -0800

    change font and margin

--- components/CalendarIntro.jsx
+++ components/CalendarIntro.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CalendarIntro = () => {
   return (
     <div className=" bg-solar-black flex items-center justify-center ">
-      <div className="w-11/12 m-32 flex self-auto flex-col justify-center items-center text-center text-4xl">
+      <div className="w-9/12 mt-32 mb-32 flex self-auto flex-col justify-center items-center text-center text-4xl">
         <p className="text-solar-gold m-0 font-ubuntu">
           Below we have our schedule for meetings!
         </p>
