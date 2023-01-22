0b67d296902088b3acaa8bbaf7da5b4e3c32dd5e
Justin Yee <justiny@solidprofessor.com>
2023-01-22 15:33:29 -0800

    Fix Subteam Responsiveness

--- components/Leads.jsx
+++ components/Leads.jsx
@@ -37,7 +37,7 @@ const Leads = () => {
     <div className=" flex items-center justify-center mb-4 ">
       <Row className="w-10/12 flex justify-center">
         {leads.map((lead, index) => (
-          <Col xl={3} md={6} xs={12} key={index} className="p-4">
+          <Col xl={3} md={6} sm={6} xs={6} key={index} className="p-4">
             <Lead
               name={lead.name}
               position={lead.position}
