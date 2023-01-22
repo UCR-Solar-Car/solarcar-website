e07a11fcc3f6f099a7745f8c957ee1951aa8d0fc
SafeDuck <schopra579@gmail.com>
2023-01-21 23:33:05 -0800

    Fixed mistake (deleted extra code)

--- components/Recruitment.jsx
+++ components/Recruitment.jsx
@@ -65,13 +65,5 @@ const Recruitment = () => {
     </div>
   );
 };
-<a
-  href="#_"
-  class="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 text-white"
->
-  <span class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-indigo-600 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-  <span class="relative text-indigo-600 transition duration-300 group-hover:text-white ease">
-    Button Text
-  </span>
-</a>;
+
 export default Recruitment;
