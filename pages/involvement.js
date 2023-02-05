c3e2a372aced90dd7c6e25538908305de98d7159
ysh0305 <yhung022@ucr.edu>
2023-02-05 15:36:15 -0800

    HI

--- pages/involvement.js
+++ pages/involvement.js
@@ -8,13 +8,22 @@ const involvement = () => {
     <div>
       <Header image="Adopt.JPG" title="Get Involved" />
       <Clubroom />
-      <Organization
-        image="WINC.PNG"
-        title="Women in Computing (WINC)"
-        ig="https://www.instagram.com/wincucr/?hl=en"
-        discord="https://discord.gg/szzhxz3S"
-        website="https://winc.cs.ucr.edu/"
-      />
+      <div className="flex flex-row justify-evenly items-center w-5/6">
+        <Organization
+          image="WINC.PNG"
+          title="Women in Computing (WINC)"
+          ig="https://www.instagram.com/wincucr/?hl=en"
+          discord="https://discord.gg/szzhxz3S"
+          website="https://winc.cs.ucr.edu/"
+        />
+        <Organization
+          image="WINC.PNG"
+          title="Women in Computing (WINC)"
+          ig="https://www.instagram.com/wincucr/?hl=en"
+          discord="https://discord.gg/szzhxz3S"
+          website="https://winc.cs.ucr.edu/"
+        />
+      </div>
     </div>
   );
 };
