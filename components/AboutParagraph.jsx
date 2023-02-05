d759a883068a9e5279cd5220ff1abdf35380a7f2
avikovi <avikovi@gmail.com>
2023-02-05 15:02:41 -0800

    Fixed formatting, cleaned images, etc

--- components/AboutParagraph.jsx
+++ components/AboutParagraph.jsx
@@ -15,7 +15,7 @@ const AboutParagraph = () => {
           solar-powered vehicles to finish a 2,000 mile cross-country journey.
         </span>
       </p>
-      <div className="bg-solar-gold w-[100%] h-10"></div>
+      <div className="bg-solar-gold"></div>
       <div className="flex">
         <img
           className="w-1/2"
