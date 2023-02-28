191edb6bf165a0eabe559e2ecbb4a3b1eeae3633
menthy-wu <wumengxuan0728@gmail.com>
2023-02-27 16:38:30 -0800

    next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const repo = "change-me-to-your-repo";
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig = {
   reactStrictMode: true,
+
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 module.exports = nextConfig;
